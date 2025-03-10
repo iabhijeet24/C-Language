331d6eae94158ca1ebab6436e04de21412681aff
theabhijeetsing <inabhijeet241107@gmail.com>
2025-03-10 21:44:28 +0530

    C language

---
+++ series3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int i, n, sum=0;
+    printf("Enter the value:");
+    scanf("%d", &n);
+    
+    for(i=2; i<=n; i=i+2)
+    {
+        sum = sum + i;
+    }
+    
+    printf("The sum is %d",sum);
+
+    return 0;
+}

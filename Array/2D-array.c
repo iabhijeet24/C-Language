52b25a2d91ed961cc1d5be2d14c570d1f9fa2cd7
Abhijeet Singh <inabhijeet241107@gmail.com>
2025-06-13 19:45:13 +0530

    Reverse

--- Array/2D-array.c
+++ Array/2D-array.c
@@ -1,1 +1,40 @@
 
+#include <stdio.h>
+
+int main() {
+    int a[2][2], b[2][2], sum[2][2];
+    int i, j, rows, cols;
+
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("Enter elements of the first matrix:\n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter elements of the second matrix:\n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    printf("Sum of the two matrices is:\n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
